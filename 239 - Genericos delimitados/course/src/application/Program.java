be3af0f2293cec2a090a56263a868da5cc843083
Rodrigo Jose Fagundes <rodrigojosefagundes@gmail.com>
2023-03-06 18:59:30 -0300

    Genéricos delimitados
    Aula 239.

---
+++ 239 - Genericos delimitados/course/src/application/Program.java
@@ -0,0 +1,43 @@
+// USANDO GENERICS DELIMITADOS PARA ENCONTRAR O MAIOR NUMERO DENTRE UM CONJUNTO
+//DE NUMEROS INTEIROS
+
+package application;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
+import entities.Product;
+import services.CalculationService;
+
+public class Program {
+
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		List<Product> list = new ArrayList<>();
+		
+		//definindo o cmainho do arquiv
+		String path = "C:\\temp\\in.txt";
+		
+		try (BufferedReader br = new BufferedReader(new FileReader(path))){
+			
+			//lendo todas as linhas do arquivo
+			String line = br.readLine();
+			while (line != null) {
+				
+				String[] fields = line.split(",");
+				
+				list.add(new Product(fields[0], Double.parseDouble(fields[1])));
+				line = br.readLine();
+			}
+			Product x = CalculationService.max(list);
+			System.out.println("Max: ");
+			System.out.println(x);
+		} catch(IOException e) {
+			System.out.println("error: " + e.getMessage());
+		}
+	}
+}
